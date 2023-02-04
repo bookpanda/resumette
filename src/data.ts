272a66846c5026fb1ce7abba63a49039c2003ff3
Idhibhat Pankam <marginpankam@gmail.com>
2023-02-04 17:44:42 +0700

    fix: add more periods.

--- src/data.ts
+++ src/data.ts
@@ -91,25 +91,25 @@ export const projects = [
   {
     name: "AutoPraeAksorn",
     details:
-      "Automatically turns images into card stunts (Prae Aksorn), inspired by Jaturamitr card stunts",
+      "Automatically turns images into card stunts (Prae Aksorn), inspired by Jaturamitr card stunts.",
     url: "auto-prae-aksorn-client.vercel.app",
   },
   {
     name: "AniTimeline",
     details:
-      "A webapp that shows data from the AniList's public API by user's ID",
+      "A webapp that shows data from the AniList's public API by user's ID.",
     url: "ani-timeline-client.vercel.app",
   },
   {
     name: "Thai2Kana",
     details:
-      "A program that tries to convert the Thai language into Katakana script using Wanakana and PyThaiNLP libraries",
+      "A program that tries to convert the Thai language into Katakana script using Wanakana and PyThaiNLP libraries.",
     url: "thai2-kana-client.vercel.app",
   },
   {
     name: "Thinc's 1-week Hack to School Project (Frontend)",
     details:
-      "Make an education platform app within a week. My job in the team was making frontend",
+      "Make an education platform app within a week. My job in the team was making frontend.",
     url: "kita.cunny.dev",
   },
   {
